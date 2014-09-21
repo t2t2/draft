eca709db0496833d18df9fdfd492b8eb9d7da534
t2t2 <taavot@gmail.com>
2014-09-21 11:40:53 +0300

    Last of foudation conversion updates, league creation page

--- app/views/league/create.blade.php
+++ app/views/league/create.blade.php
@@ -1,18 +1,28 @@
 @extends('layout.leagues')
 
 @section('layout.content')
-	<h1>Create a league</h1>
+	<h2>Create a league</h2>
 
-	{{ Former::open()->route('league.store')->rules($validation_rules) }}
-		{{ Former::text('name') }}
-		{{ Former::textarea('description') }}
-		{{ Former::text('url') }}
-		{{ Former::checkbox('private')->unchecked_value(0) }}
+	{{ Former::vertical_open()->route('league.store')->rules($validation_rules) }}
 
-		{{ Former::legend('League Settings') }}
-		{{ Former::text('money')->value(Config::get('draft.league_defaults.money')) }}
-		{{ Former::text('units')->value(Config::get('draft.league_defaults.units')) }}
-		{{ Former::number("extra_weeks")->range(1, 12)->value(Config::get("draft.league_defaults.extra_weeks")) }}
+		<fieldset>
+			{{ Former::legend('League info') }}
+			{{ Former::text('name') }}
+			{{ Former::textarea('description')->rows(4) }}
+			<div class="row">
+				{{ Former::text('url')->addGroupClass('medium-10 column') }}
+				{{ Former::checkbox('private')->unchecked_value(0)->addGroupClass('medium-2 column') }}
+			</div>
+		</fieldset>
+
+		<fieldset>
+			{{ Former::legend('League Settings') }}
+			<div class="row">
+				{{ Former::text('money')->value(Config::get('draft.league_defaults.money'))->addGroupClass('medium-4 columns') }}
+				{{ Former::text('units')->value(Config::get('draft.league_defaults.units'))->addGroupClass('medium-4 columns') }}
+				{{ Former::number("extra_weeks")->range(1, 12)->value(Config::get("draft.league_defaults.extra_weeks"))->addGroupClass('medium-4 columns') }}
+			</div>
+		</fieldset>
 
 		{{ Former::actions()->primary_submit('Submit')->reset('Reset') }}
 	{{ Former::close() }}
