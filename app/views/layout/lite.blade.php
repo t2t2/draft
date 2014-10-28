40fb4585f3d21162d561c20b52955e085053fc32
t2t2 <taavot@gmail.com>
2014-10-28 03:59:45 +0200

    All your tracks are belong to me

--- app/views/layout/lite.blade.php
+++ app/views/layout/lite.blade.php
@@ -23,6 +23,22 @@
 	<script src="https://login.persona.org/include.js"></script>
 
 	<script src="{{ asset("js/main.js") }}"></script>
-
+	@if(App::environment() == 'production')
+		<!-- Piwik -->
+        <script type="text/javascript">
+          var _paq = _paq || [];
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//stats.t2t2.eu/";
+            _paq.push(['setTrackerUrl', u+'piwik.php']);
+            _paq.push(['setSiteId', 1]);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+        <noscript><p><img src="//stats.t2t2.eu/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+        <!-- End Piwik Code -->
+	@endif
 </body>
 </html>
