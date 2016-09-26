59e6e2a76cd8be8e58a8f12f60e0b816a0934910
Mike Dorval <rockingdorval@gmail.com>
2016-09-26 03:36:16 -0400

    Added the ability to edit existing movies (for title/release date changes) Fixed a bug where movies in 2017 couldn't be added Paginated the Movies list

--- app/views/admin/addmovie.blade.php
+++ app/views/admin/addmovie.blade.php
@@ -8,18 +8,18 @@
 		{{ Form::text('movie',$info->mojo_id) }}
 		<button type="submit">Check Movie</button>
     {{Form::close()}}
-	<?php if($info->found) {?>
+@if($info->found)
 	{{ Form::open(['route' => ['admin.confirmMovie']]) }}
 	Movie Information:
 	<table>
 		<tr><td>Title</td><td><?=$info->title?></td></tr>
 		<tr><td>Release Date</td><td><?=$info->release_date?></td></tr>
 		<tr><td>Boxmojo Id</td><td><?=$info->mojo_id?></td></tr>
-		</tr>
 	</table>
 	{{Form::hidden('title',$info->title)}}
 	{{Form::hidden('release_date',$info->release_date)}}
 	{{Form::hidden('mojo_id',$info->mojo_id)}}
+@if($info->grosses)
 	Existing Gross:
 	<table>
 		<tr><th>Date</th><th>Gross</th><tr>
@@ -28,8 +28,17 @@
 		<?php } ?>
 	</table>
 	{{Form::hidden('grosses',serialize($info->grosses))}}
+@endif
+@if($info->old)
+	Existing Movie Info:
+	<table>
+		<tr><td>Title</td><td><?=$info->old->name?></td></tr>
+		<tr><td>Release Date</td><td><?=$info->old->release?></td></tr>
+	</table>
+	{{Form::hidden('old_id',$info->old->id)}}
+@endif
 	<button type="submit">Confirm Movie</button>
 	{{Form::close()}}
-	<?php } ?>
+@endif
 	
 @endsection
